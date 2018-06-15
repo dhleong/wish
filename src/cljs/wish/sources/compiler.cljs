e5b703e16f821192810c00d85e44ae19b959f366
Daniel Leong <me@dhleong.net>
2018-06-14 20:20:39 -0400

    Add preliminary options support

--- src/cljs/wish/sources/compiler.cljs
+++ src/cljs/wish/sources/compiler.cljs
@@ -4,22 +4,48 @@
   (:require [wish.sources.compiler.feature :refer [compile-feature]]
             [wish.templ.fun :refer [->callable]]))
 
+; ======= options ==========================================
+
+(defn compile-option
+  [o]
+  (if (contains? o :!)
+    (compile-feature o)
+    o))
+
 ; ======= directives =======================================
 
 (def directives
-  {:!provide-feature
+  {:!declare-class
+   (fn declare-class [state class-map]
+     (update state :classes
+             assoc
+             (:id class-map) class-map))
+
+   :!provide-feature
    (fn provide-feature [state & args]
-     (let [feature-maps (filter map? args)
+     (let [features (->> args
+                         (filter map?)
+                         (map compile-feature))
            filters (filter vector? args)]
-       ; TODO filters?
-       (->> feature-maps
-            (map compile-feature)
+       ; TODO apply filters?
+       (->> features
             (reduce
               (fn [s feature-map]
                 (update s :features
                         assoc
                         (:id feature-map) feature-map))
               state))))
+
+   :!provide-options
+   (fn provide-options [state feature-id & option-maps]
+     (let [options (map compile-option option-maps)]
+       (if (get-in state [:features feature-id])
+         (update-in state [:features feature-id :values]
+                    concat options)
+
+         ;; TODO
+         (update-in state [:deferred-options feature-id]
+                    concat options))))
    })
 
 (defn apply-directive [state directive-vector]
@@ -33,14 +59,30 @@
         (println "WARN: unknown directive:" kind)
         state))))
 
+(defn- install-deferred-options
+  [s]
+  (let [opts (:deferred-options s)]
+    (reduce
+      (fn [state [feature-id options]]
+        (update-in state [:features feature-id :values]
+                   concat options))
+      (dissoc s :deferred-options)
+      opts)))
 
 ; ======= public api =======================================
 
 (defn compile-directives
   "Given a sequence of directives, return a compiled DataSource state"
   [directives]
-  (reduce
-    apply-directive
-    {}
-    directives))
+  (->> directives
+
+       ; compile directives into a state
+       (reduce apply-directive {})
+
+       ; install options
+       install-deferred-options
+
+       ; TODO more deferred processing, now that we have all directives
+       ; EG: provide all filtered features to matching classes
+       ))
 
--- test/cljs/wish/sources/compiler_test.cljs
+++ test/cljs/wish/sources/compiler_test.cljs
@@ -15,3 +15,19 @@
       (is (contains? s :features))
       (is (contains? (:features s)
                      :hit-dice/d10)))))
+
+(deftest options-test
+  (testing "Options provided before the feature"
+    (let [s (compile-directives
+              [[:!provide-options
+                :feature
+                {:id :feature/opt1}
+                {:id :feature/opt2}]
+
+               [:!provide-feature
+                {:id :feature
+                 :max-options 1}]])
+          f (get-in s [:features :feature])]
+      (is (= [{:id :feature/opt1}
+              {:id :feature/opt2}]
+             (:values f))))))
