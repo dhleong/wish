cd83f1c1ed8aad417f7a7187ac5551b79eef0437
Daniel Leong <me@dhleong.net>
2018-07-04 16:36:39 -0400

    Add externs for new Google APIs

--- externs/gapi.js
+++ externs/gapi.js
@@ -18,6 +18,7 @@ gapi.client.init = function() {};
 gapi.client.drive.files.get = function() {};
 gapi.client.drive.files.delete = function() {};
 gapi.client.drive.files.list = function() {};
+gapi.client.drive.files.update = function() {};
 
 /** @return {gapi.ClientRequest} */
 gapi.client.request = function() {};
@@ -31,6 +32,58 @@ gapi.AuthInstance.prototype.isSignedIn = {
 gapi.AuthInstance.prototype.signIn = function() {};
 gapi.AuthInstance.prototype.signOut = function() {};
 
+gapi.AuthInstance.prototype.currentUser = {
+    /** @return {gapi.auth2.GoogleUser} */
+    get: function() {},
+};
+
 /** @constructor */
 gapi.ClientRequest = function() {};
 gapi.ClientRequest.prototype.execute = function() {};
+
+/** @constructor */
+gapi.auth2.GoogleUser = function() {};
+
+/** @return {gapi.auth2.AuthResponse} */
+gapi.auth2.GoogleUser.prototype.getAuthResponse = function() {};
+
+/** @constructor */
+gapi.auth2.AuthResponse = function() {};
+
+/** @type {string} */
+gapi.auth2.AuthResponse.access_token;
+
+
+var google = {
+    picker: {
+        ViewId: {
+            DOCS: "all",
+        },
+    },
+};
+
+/** @constructor */
+google.picker.View = function() {};
+google.picker.View.prototype.setMimeTypes = function() {};
+
+/** @constructor */
+google.picker.DocsUploadView = function() {};
+
+/** @constructor */
+google.picker.PickerBuilder = function() {};
+
+/** @return {google.picker.PickerBuilder} */
+google.picker.PickerBuilder.prototype.addView = function() {};
+/** @return {google.picker.PickerBuilder} */
+google.picker.PickerBuilder.prototype.setAppId = function() {};
+/** @return {google.picker.PickerBuilder} */
+google.picker.PickerBuilder.prototype.setOauthToken = function() {};
+/** @return {google.picker.PickerBuilder} */
+google.picker.PickerBuilder.prototype.setCallback = function() {};
+
+/** @return {google.picker.Picker} */
+google.picker.PickerBuilder.prototype.build = function() {};
+
+/** @constructor */
+google.picker.Picker = function() {};
+google.picker.setVisible = function() {};
