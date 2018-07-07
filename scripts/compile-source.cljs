3a2820a9bccd50e9c4fcfdd2402df75eddd47ebc
Daniel Leong <me@dhleong.net>
2018-07-06 20:37:34 -0400

    Update source compiler to work for custom sources
    Also adds an extra script that compiles all builtin sources

--- scripts/compile-source.cljs
+++ scripts/compile-source.cljs
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-"exec" "plk" "-Sdeps" "{:deps {org.clojure/tools.cli {:mvn/version \"0.3.5\"}}}" "-sf" "$0" "$@" "dnd5e"
+"exec" "plk" "-Sdeps" "{:deps {org.clojure/tools.cli {:mvn/version \"0.3.5\"}}}" "-sf" "$0" "$@"
 (ns wish.builtin-source-compiler
   (:require [clojure.tools.cli :refer [parse-opts]]
             [clojure.tools.reader.reader-types :refer [source-logging-push-back-reader]]
@@ -7,13 +7,23 @@
             [cljs.reader :as edn]
             [planck.core :refer [exit file-seq *err* slurp with-open]]
             [planck.environ :refer [env]]
-            [planck.io :refer [directory? file writer]]))
+            [planck.io :as io :refer [directory? file writer]]))
 
 (defn log
   [& args]
   (binding [*out* *err*]
     (apply println args)))
 
+(defn- normalize-file-path [f]
+  (str/replace (.-path f) #"^\./" ""))
+
+(defn files-same?
+  "Planck's File is not very useful; this is a better equality
+   function"
+  [a b]
+  (let [normalized-a (normalize-file-path a)
+        normalized-b (normalize-file-path b)]
+    (= normalized-a normalized-b)))
 
 ; ======= validation =======================================
 
@@ -37,12 +47,13 @@
       ; keep loading directives
       (recur reader))))
 
-(defn process-source [source-dir source-name]
+(defn process-source [source-dir source-name dest-file]
   (let [files-to-process (->> source-dir
                               file-seq
                               (filter (complement directory?))
-                              (remove #(let [p (.-path %)]
-                                         (re-matches #".*\.sw[nop]$" p)))
+                              (remove #(str/includes? (.-path %) ".git"))
+                              (filter #(str/ends-with? (.-path %) ".edn"))
+                              (remove #(files-same? dest-file %))
                               (sort-by #(.-path %)))]
     (doseq [f files-to-process]
       (log "Processing " (.-path f) "...")
@@ -61,28 +72,44 @@
    ; help
    ["-h" "--help"]])
 
-(def usage-str "usage: scripts/compile-builtin-source.cljs [source-dir-name]")
+(def usage-str "usage:
+  scripts/compile-builtin-source.cljs [source-dir-name]
+  scripts/compile-builtin-source.cljs [source-dir] [output-file]
+")
 
 (defn -main [& args]
-  (println args)
   (let [{:keys [options arguments summary errors]} (parse-opts (map str args) cli-options
                                                                :in-order false)
+
+        mode (case (count args)
+               1 :builtin
+               2 :custom
+               3 :custom ; this is hax because right now we insert the dnd5e as the last arg
+               (println "Unexpected args: " args))
+
         source-dir-name (first arguments)
         root (str/replace (:pwd env)
                           #"\/scripts$"
                           "")
-        source-dir (when source-dir-name
-                     (-> root
-                         (file "resources")
-                         (file "sources")
-                         (file source-dir-name)))
 
-        dest-file (when source-dir-name
-                    (-> root
-                        (file "resources")
-                        (file "public")
-                        (file "sources")
-                        (file (str source-dir-name ".edn"))))]
+        source-dir (case mode
+                     :builtin (when source-dir-name
+                                (-> root
+                                    (file "resources")
+                                    (file "sources")
+                                    (file source-dir-name)))
+                     :custom (when-let [src (first arguments)]
+                               (file src)))
+
+        dest-file (case mode
+                    :builtin (when source-dir-name
+                               (-> root
+                                   (file "resources")
+                                   (file "public")
+                                   (file "sources")
+                                   (file (str source-dir-name ".edn"))))
+                    :custom (when-let [dst (second arguments)]
+                              (file dst)))]
 
     (when errors
       (println errors)
@@ -108,11 +135,11 @@
 
     (if (:stdout options)
       ; just dump everything to stdout
-      (process-source source-dir source-dir-name)
+      (process-source source-dir source-dir-name dest-file)
 
       ; open the output file and bind to *out*
       (with-open [source-out (writer dest-file)]
         (binding [*out* source-out]
-          (process-source source-dir source-dir-name))))))
+          (process-source source-dir source-dir-name dest-file))))))
 
 (set! *main-cli-fn* -main)
