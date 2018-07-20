6520e8dbc9290f994d7a908403be7abf0bffc03e
Daniel Leong <me@dhleong.net>
2018-07-20 08:40:57 -0400

    Add a bunch of dangling tests to the runner

--- test/cljs/wish/runner.cljs
+++ test/cljs/wish/runner.cljs
@@ -1,20 +1,29 @@
 (ns wish.runner
-    (:require [doo.runner :refer-macros [doo-tests]]
-              [wish.events-test]
-              [wish.sheets.dnd5e.events-test]
-              [wish.sheets.dnd5e.subs-test]
-              [wish.sheets.dnd5e.util-test]
-              [wish.sources.compiler-test]
-              [wish.sources.compiler.entity-mod-test]
-              [wish.sources.compiler.feature-test]
-              [wish.sources.compiler.fun-test]
-              [wish.sources.compiler.limited-use-test]
-              [wish.sources.compiler.lists-test]
-              [wish.util-test]
-              [wish.util.formatted-test]))
+  (:require [doo.runner :refer-macros [doo-tests]]
+            [wish.events-test]
+            [wish.inventory-test]
+            [wish.sheets.dnd5e.builder-test]
+            [wish.sheets.dnd5e.data-test]
+            [wish.sheets.dnd5e.events-test]
+            [wish.sheets.dnd5e.overlays-test]
+            [wish.sheets.dnd5e.subs-test]
+            [wish.sheets.dnd5e.util-test]
+            [wish.sources.compiler-test]
+            [wish.sources.compiler.entity-mod-test]
+            [wish.sources.compiler.feature-test]
+            [wish.sources.compiler.fun-test]
+            [wish.sources.compiler.limited-use-test]
+            [wish.sources.compiler.lists-test]
+            [wish.util-test]
+            [wish.util.formatted-test]
+            [wish.views.sheet-builder-util-test]))
 
 (doo-tests 'wish.events-test
+           'wish.inventory-test
+           'wish.sheets.dnd5e.builder-test
+           'wish.sheets.dnd5e.data-test
            'wish.sheets.dnd5e.events-test
+           'wish.sheets.dnd5e.overlays-test
            'wish.sheets.dnd5e.subs-test
            'wish.sheets.dnd5e.util-test
            'wish.sources.compiler-test
@@ -24,4 +33,6 @@
            'wish.sources.compiler.limited-use-test
            'wish.sources.compiler.lists-test
            'wish.util-test
-           'wish.util.formatted-test)
+           'wish.util.formatted-test
+           'wish.views.sheet-builder-util-test
+           )
