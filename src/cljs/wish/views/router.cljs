8d2507c1c87876831ab697e7e2e8e1fb1b988ce2
Daniel Leong <me@dhleong.net>
2018-08-26 11:11:51 -0400

    Add links to the Github repo and Issue tracker in the footer

--- src/cljs/wish/views/router.cljs
+++ src/cljs/wish/views/router.cljs
@@ -21,9 +21,18 @@
 
 (defn footer []
   [:footer.footer
-   [:a {:href (str server-root "/privacy.html")
-        :target '_blank}
-    "Privacy Policy"]])
+   [:a.link {:href (str server-root "/privacy.html")
+             :target '_blank}
+    "Privacy Policy"]
+
+   [:a.link {:href "https://github.com/dhleong/wish"
+             :target '_blank}
+    "Contribute"]
+
+   [:a.link {:href "https://github.com/dhleong/wish/issues"
+             :target '_blank}
+    "Issues"]
+   ])
 
 (defn router
   "Renders the current page, given a map
