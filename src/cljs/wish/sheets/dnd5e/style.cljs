556e14e138cbba4969033293d16542f4a05da0bf
Daniel Leong <me@dhleong.net>
2018-08-30 00:05:43 -0400

    Fix (apparently) missing require for cljs-css-modules

--- src/cljs/wish/sheets/dnd5e/style.cljs
+++ src/cljs/wish/sheets/dnd5e/style.cljs
@@ -1,7 +1,8 @@
 (ns ^{:author "Daniel Leong"
       :doc "dnd5e.style"}
   wish.sheets.dnd5e.style
-  (:require [garden.color :as color]
+  (:require [cljs-css-modules.macro :as css]
+            [garden.color :as color]
             [wish.style :refer-macros [defclass defstyled]]
             [wish.style.flex :as flex :refer [flex]]
             [wish.style.shared :refer [metadata]]))
