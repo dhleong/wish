9ee351ea2940cb7670ed0c53fa55cc5864a5dda6
Daniel Leong <me@dhleong.net>
2018-09-08 18:01:06 -0400

    Remove the compiled worker.js

--- resources/public/worker.js
+++
@@ -1,8 +0,0 @@
-var CLOSURE_BASE_PATH = "js/compiled/worker-out/goog/";
-var CLOSURE_UNCOMPILED_DEFINES = {"cljs.core._STAR_target_STAR_":"webworker"};
-var CLOSURE_IMPORT_SCRIPT = (function(global) { return function(src) {global['importScripts'](src); return true;};})(this);
-if(typeof goog == 'undefined') importScripts("js/compiled/worker-out/goog/base.js");
-importScripts("js/compiled/worker-out/cljs_deps.js");
-goog.require("figwheel.preload");
-goog.require("process.env");
-goog.require("wish.worker.core");
