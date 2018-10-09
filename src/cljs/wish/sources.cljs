63529e0463df70489cb5a7fa3a62811308829de6
Daniel Leong <falcone88@gmail.com>
2018-10-08 22:25:35 -0400

    Support Transit sources (#87)
    * Experiment with using transit to load WISH builtin sources
    
    Refs #9
    
    * Use wish-compiler to compile transit Data Sources
    
    Refs #9
    
    Also removes all the cruft from the travis config to build and use
    planck for this task
    
    * Use a very simple heuristic to choose transit vs edn for data sources
    
    Refs #9
    
    * Add more logging to scripts/wish-compiler

--- src/cljs/wish/sources.cljs
+++ src/cljs/wish/sources.cljs
@@ -6,6 +6,7 @@
   (:require [clojure.core.async :as async :refer [alts! <!]]
             [clojure.tools.reader.reader-types :refer [string-push-back-reader]]
             [cljs.reader :as edn]
+            [cognitect.transit :as t]
             [wish.providers :as providers]
             [wish.sheets :as sheets]
             [wish.sources.compiler :refer [compile-directives]]
@@ -16,19 +17,34 @@
 ; cache of *compiled* sources by id
 (defonce ^:private loaded-sources (atom {}))
 
-(defn- compile-raw-source
-  [{:keys [kind] :as sheet} id raw]
+(defn- read-transit-directives [raw]
+  (t/read (t/reader :json) raw))
+
+(defn- read-edn-directives [raw]
   (loop [reader (string-push-back-reader raw)
          directives []]
     (if-let [d (edn/read reader)]
       ; keep loading directives
       (recur reader (conj directives d))
 
-      (->DataSource
-        id
-        (->> directives
-             (compile-directives)
-             (sheets/post-compile kind))))))
+      ; done!
+      directives)))
+
+(defn- compile-raw-source
+  [{:keys [kind] :as sheet} id raw]
+  (let [directives (if (= (subs raw 0 2) "[[")
+                     (do
+                       (log "Read transit for " id)
+                       (read-transit-directives raw))
+
+                     (do
+                       (log "Read edn for " id)
+                       (read-edn-directives raw)))]
+    (->DataSource
+      id
+      (->> directives
+           (compile-directives)
+           (sheets/post-compile kind)))))
 
 (defn- load-source!
   "Returns a channel that signals with [err] or [nil source] when done"
