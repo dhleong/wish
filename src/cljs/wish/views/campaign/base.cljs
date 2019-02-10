c3bbf4279f43c153f80ef59766624914538ba427
Daniel Leong <me@dhleong.net>
2019-02-09 20:47:34 -0500

    Add some placeholders and a link to the Campaign UI ticket
    It's about time for this branch to get merged; there's a ton of
    refactoring in it already; all the remaining features can be done
    in one or more seperate branches.
    
    The only thing we really need to do before merging is allow players
    to detach the campaign from their character.

--- src/cljs/wish/views/campaign/base.cljs
+++ src/cljs/wish/views/campaign/base.cljs
@@ -9,4 +9,16 @@
   [error-boundary
    [:div.campaign
     [chars-carousel char-card]
-    "TODO campaign"]])
+
+    [:div.info
+     "This is the campaign page."
+
+     [:div.group
+      [:a {:href "https://github.com/dhleong/wish/issues/69"
+           :target '_blank}
+       "More will be coming here"]
+      "... eventually."]
+
+     [:div.group
+      "In the meantime, use the + button above to add characters to this campaign."]
+     ]]])
