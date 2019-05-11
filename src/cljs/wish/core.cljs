34d79dfc6ab334828fa57bd156fbbec49e45cbbd
Daniel Leong <me@dhleong.net>
2019-05-11 17:00:53 -0400

    Mount app routes after initializing DB
    Fixes issue on iOS/Safari where the initial navigation gets lost and it
    gets stuck on the splash screen.

--- src/cljs/wish/core.cljs
+++ src/cljs/wish/core.cljs
@@ -35,9 +35,9 @@
                   (.getElementById js/document "app")))
 
 (defn ^:export init []
-  (routes/app-routes)
   (re-frame/dispatch-sync [::events/initialize-db])
   (re-frame/dispatch-sync [::rp/add-keyboard-event-listener "keydown"])
+  (routes/app-routes)
   (dev-setup)
   (mount-worker)
   (mount-root :first!))
