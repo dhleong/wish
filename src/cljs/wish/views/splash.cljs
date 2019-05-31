e59511dfc3be836fcc56a62b5249052a17a8962e
Daniel Leong <me@dhleong.net>
2019-05-30 23:55:41 -0400

    Embed splash in the HTML for more immediate rendering
    Closes #108

--- src/cljs/wish/views/splash.cljs
+++ src/cljs/wish/views/splash.cljs
@@ -1,24 +1,10 @@
 (ns ^{:author "Daniel Leong"
       :doc "Splash screen"}
   wish.views.splash
-  (:require [wish.config :as config]
-            [wish.style :refer [defstyled]]
-            [wish.style.flex :as flex :refer [flex]]))
-
-(defstyled splash-container
-  (merge flex
-         flex/justify-center
-         {:height "100vh"}))
-
-(defstyled splash
-  (merge flex/vertical-center
-         flex/center)
-  [:img.logo {:height "72px"
-              :width "72px"
-              :margin "12px"}])
+  (:require [wish.config :as config]))
 
 (defn page []
-  [:div splash-container
-   [:div splash
+  [:div#splash-container
+   [:div.splash
     [:img.logo {:src (str config/server-root "/assets/icon/icon-192.png")}]
     "WISH"]])
