9e0287654c4e68259f09cb9492d4bcf4f29787f8
Daniel Leong <me@dhleong.net>
2019-10-05 16:03:26 -0400

    Begin tweaking note save format

--- src/cljs/wish/subs/campaign/notes.cljs
+++ src/cljs/wish/subs/campaign/notes.cljs
@@ -2,13 +2,26 @@
   (:require [re-frame.core :refer [reg-sub]]
             [wish.subs.campaign.base]))
 
+(defn- inflate-note [n id]
+  {:id id
+   :modified (:m n)
+   :name (:n n)
+   :contents (:c n)})
+
 (reg-sub
   ::by-id
   :<- [:meta/notes]
   (fn [notes-map]
     (reduce-kv
       (fn [m id note]
-        ; TODO inflate
-        (assoc m id note))
+        (assoc m id (inflate-note note id)))
       {}
       notes-map)))
+
+(reg-sub
+  ::sorted
+  :<- [::by-id]
+  (fn [notes-map]
+    (->> notes-map
+         vals
+         (sort-by :modified))))
--- src/cljs/wish/views/campaign/pages/notes.cljs
+++ src/cljs/wish/views/campaign/pages/notes.cljs
@@ -1,5 +1,14 @@
-(ns wish.views.campaign.pages.notes)
+(ns wish.views.campaign.pages.notes
+  (:require [wish.subs.campaign.notes :as notes]
+            [wish.util :refer [<sub]]))
+
+(defn- notes-list [notes]
+  [:<>
+   (for [n notes]
+     [:div.note (str n)])])
 
 (defn page []
-  [:<>
-   "Notes"])
+  (let [notes (<sub [::notes/sorted])]
+    (if-not (seq notes)
+      [:div "No notes"]
+      [notes-list notes])))
