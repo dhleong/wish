448372d63b4212f2d3fb5955b52079f9fd344b05
Daniel Leong <me@dhleong.net>
2019-10-16 11:32:00 -0400

    Update error-boundary to auto re-render after-load

--- src/cljs/wish/views/error_boundary.cljs
+++ src/cljs/wish/views/error_boundary.cljs
@@ -3,6 +3,18 @@
   wish.views.error-boundary
   (:require [reagent.core :as r]))
 
+(when goog.DEBUG
+  ; in debug builds, we can auto-retry rendering error'd components
+  ; every time a load occurs
+  (def ^:private active-err-atoms (atom #{}))
+
+  (defn ^:dev/after-load clear-errors []
+    (swap! active-err-atoms (fn [atoms]
+                              (doseq [a atoms]
+                                (reset! a nil))
+
+                              ; clear
+                              #{}))))
 (defn error-boundary [& _]
   (r/with-let [err (r/atom nil)]
     (r/create-class
@@ -11,6 +23,14 @@
       :component-did-catch (fn [_this error #_info]
                              ;; (js/console.warn error info)
                              (reset! err error))
+
+       :statics
+       #js {:getDerivedStateFromError (fn [error]
+                                        (when goog.DEBUG
+                                          ; enqueue the atom for auto-clearing
+                                          (swap! active-err-atoms conj err))
+
+                                        (reset! err error))}
 
       :reagent-render (fn [& children]
                          (if-let [e @err]
