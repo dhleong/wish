155ce9659f9546e24eee81418269d9e1db2762b9
Daniel Leong <me@dhleong.net>
2020-12-30 10:50:48 -0500

    Update tests to reflect moved function

--- test/cljs/wish/events_test.cljs
+++ test/cljs/wish/events_test.cljs
@@ -1,32 +1,8 @@
 (ns wish.events-test
   (:require [cljs.test :refer-macros [deftest testing is]]
             [wish.events :refer [apply-limited-use-trigger
-                                 restore-trigger-matches?
                                  inventory-add
                                  inventory-subtract]]))
-
-(deftest restore-trigger-matches
-  (testing "Keyword case is simple"
-    (is (true? (restore-trigger-matches?
-                 :required
-                 :required)))
-    (is (false? (restore-trigger-matches?
-                 :required
-                 :other))))
-  (testing "Sets are handled"
-    (is (true? (restore-trigger-matches?
-                 #{:required}
-                 :required)))
-    (is (false? (restore-trigger-matches?
-                 #{:required}
-                 :other))))
-  (testing "Other collections work"
-    (is (true? (restore-trigger-matches?
-                 [:required]
-                 :required)))
-    (is (false? (restore-trigger-matches?
-                 [:required]
-                 :other)))))
 
 (deftest apply-limited-use-trigger-test
   (testing "Apply matching trigger"
